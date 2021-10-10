42d7839f10c291f8ab18be350009ecb5a299b38e
Rastislav Budinsky <rastislavbudinsky@gmail.com>
2021-10-10 11:57:39 +0200

    Add doxy documentation for file&struct, add new functions for Element interface, add implementations to .c file

--- src/Utils/symbolelement.c
+++ src/Utils/symbolelement.c
@@ -1,10 +1,25 @@
+/**
+ * @file symbolelement.c
+ * @brief This file implements whole interface of symbolelement.h
+ * @authors Denis Pojezdal, Rastislav Budinsky
+ */
 #include "symbolelement.h"
+#include "logger.h"
 
 #include <stdlib.h>
 
-Element* Element_Init(char* key, SymbolType type, int isDefined, void* data)
+Element* Element_Init(char* key, SymbolType type, char isDefined, void* data)
 {
-	return NULL;
+	Element *element = (Element*) malloc(sizeof(Element));
+	if (element == NULL)
+		ERROR("Allocation failed!\n");
+
+	element->key = key;
+	element->type = type;
+	element->isDefined = isDefined;
+	element->data = data;
+	
+	return element;
 }
 
 void Element_Free(Element* element)
@@ -13,4 +28,25 @@ void Element_Free(Element* element)
 	free(element->data);
 	// TO DO maybe free the key value, but it might be shared amongs all elements of stack
 	return;
+}
+
+
+char *Element_GetKey(Element *element)
+{
+	return element->key;
+}
+
+char Element_IsDefined(Element *element)
+{
+	return element->isDefined;
+}
+
+void Element_Define(Element *element)
+{
+	element->isDefined = 1;
+}
+
+void *Element_GetData(Element *element)
+{
+	element->data;
 }
--- src/Utils/symbolelement.h
+++ src/Utils/symbolelement.h
@@ -1,3 +1,9 @@
+/**
+ * @file symbolelement.h
+ * @brief This file contains struct for element from Symbol Table and
+ * 	interface for working with this struct
+ * @authors Denis Pojezdal, Rastislav Budinsky
+ */
 #ifndef __SYMBOL_ELEMENT__
 #define __SYMBOL_ELEMENT__
 
@@ -9,22 +15,22 @@ typedef enum SymbolType
 
 typedef struct SymbolElement
 {
-	SymbolType type;
-	int isDefined;
-	char* key;
-	void* data;
+	SymbolType type;	//! type of Symbol Element for further casting and destructors
+	char isDefined;		//! bool value deciding whether Symbol was defined
+	char* key;			//! pointer to the string with key, currently is not allocated in SymbolElement
+	void* data;			//! pointer to the data, should be casted
 
 } Element;
 
 /**
- * @brief 
+ * @brief Allocates memory and initializes it according to arguments
  * @param key 
  * @param type 
  * @param isDefined 
  * @param data 
  * @return 
 */
-Element* Element_Init(char* key, SymbolType type, int isDefined, void* data);
+Element* Element_Init(char* key, SymbolType type, char isDefined, void* data);
 
 /**
  * @brief Frees all allocated memory
@@ -32,6 +38,31 @@ Element* Element_Init(char* key, SymbolType type, int isDefined, void* data);
 */
 void Element_Free(Element* element);
 
+/**
+ * @brief returns pointer allocated string by the struct disctinguishing identifiers
+ * @param element the Element we want to get info about
+ * @return Pointer to the first char
+ */
+char *Element_GetKey(Element *element);
 
+/**
+ * @brief
+ * @param element the Element we want to get info about
+ * @return returns 1 upon defined Symbol Element, otherwise 0
+ */
+char Element_IsDefined(Element *element);
+
+/**
+ * @brief Set define to 1 regardless of previous state
+ * @param element the Element we want to change info of
+ */
+void Element_Define(Element *element);
+
+/**
+ * @brief
+ * @param element the Element we want to get info about
+ * @return the pointer to the stored data
+ */
+void *Element_GetData(Element *element);
 
 #endif // !__SYMBOL_ELEMENT__
