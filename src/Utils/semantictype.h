a019068a3dda7f69ab3ca7f7a1a173e8418be845
Rastislav Budinsky <rastislavbudinsky@gmail.com>
2021-11-19 17:54:01 +0100

    Add header wrapping

--- src/Utils/semantictype.h
+++ src/Utils/semantictype.h
@@ -4,4 +4,9 @@
  * 
  * @brief This file contains enum for Semantic Analyzer
  */
+#ifndef __SEMANTIC_TYPE_H__
+#define __SEMANTIC_TYPE_H__
+
 typedef enum {SEMANTIC_INTEGER, SEMANTIC_NUMBER, SEMANTIC_BOOLEAN, SEMANTIC_STRING, SEMANTIC_VOID}SemanticType;
+
+#endif //!__SEMANTIC_TYPE_H__
